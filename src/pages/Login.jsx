8c08c01adb4ac523e6cbe9a326f2e45619087466
ManvithaDantuluri <2300032118cseh@gmail.com>
2025-11-08 11:26:10 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,8 +10,8 @@ export default function Login() {
   // ✅ Base URL for backend (using HTTPS on port 30083)
   const BASE_URL =
     window.location.hostname === "localhost"
-      ? "https://localhost:30083"
-      : "https://backend:30083";
+      ? "http://localhost:30083"
+      : "http://backend:30083";
 
   const handleLogin = async (e) => {
     e.preventDefault();
